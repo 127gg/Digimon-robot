9a9a1727df7c535ef6669f769eac87ce494b900a
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-03 04:51:36 +0700

    Update repo.py

--- KanekiRobot/modules/repo.py
+++ KanekiRobot/modules/repo.py
@@ -24,10 +24,7 @@ async def repo(_, message):
 | Contributors |
 ----------------```
 {list_of_users}
-InlineKeyboardMarkup(
-                        [[InlineKeyboardButton(text="[💾 Repository ]", url=f"https://github.com/rizkyoye/Kaneki-robot")]]
-                    ),
-                )"""
+[💾 Repository](https://github.com/rizkyoye/Kaneki-robot)"""
     await app.send_message(
         message.chat.id, text=text, disable_web_page_preview=True
     )
